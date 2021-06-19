50b1bf856f4000aceceb58b1895f42a3fc835a06
Ahmed Saad  Mansouri <ahmeds.mansouri@imedia24.de>
2021-06-19 12:34:17 +0100

    added tsx

--- reactComponentInit.sh
+++ reactComponentInit.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
 
-if [ $# -ne 4 ]; then 
-	echo 'Usage: ./reactComponentInit [ComponentDirectory] [ComponentName] [class - function] [css, scss, none]'
+if [ $# -ne 5 ]; then 
+	echo 'Usage: ./reactComponentInit [ComponentDirectory] [ComponentName] [class - function] [css, scss, none] [tsx, js]'
 else
 	CMPNTDIR=$1
 	CMPNTNAME=$2
 	CMPNTTYPE=$3
 	CMPNTSTYLE=$4
+	CMPNTEX=$5
 
+	if [ "$CMPNTEX" == "tsx" ]; then
+		CMPLNAME="$CMPNTNAME.tsx"
+	fi
+	if [ "$CMPNTEX" == "js" ]; then
+		CMPLNAME="$CMPNTNAME.js"
+	fi
 	mkdir $CMPNTDIR/$CMPNTNAME; 
-	touch $CMPNTDIR/$CMPNTNAME/$CMPNTNAME.js; 
-	code $CMPNTDIR/$CMPNTNAME/$CMPNTNAME.js
+	touch $CMPNTDIR/$CMPNTNAME/$CMPLNAME
+	code $CMPNTDIR/$CMPNTNAME/$CMPLNAME
 	if [ "$CMPNTSTYLE" == "css" ]; then
 		touch $CMPNTDIR/$CMPNTNAME/$CMPNTNAME.module.css
-		echo "import classes from './$CMPNTNAME.module.css';" >> $CMPNTDIR/$CMPNTNAME/$CMPNTNAME.js
+		echo "import classes from './$CMPNTNAME.module.css';" >> $CMPNTDIR/$CMPNTNAME/$CMPLNAME
 		echo ".$CMPNTNAME {
 }" >> $CMPNTDIR/$CMPNTNAME/$CMPNTNAME.module.css
 		code $CMPNTDIR/$CMPNTNAME/$CMPNTNAME.module.css
 	fi
 	if [ "$CMPNTSTYLE" == "scss" ]; then
 		touch $CMPNTDIR/$CMPNTNAME/$CMPNTNAME.module.scss
-		echo "import classes from './$CMPNTNAME.module.scss';" >> $CMPNTDIR/$CMPNTNAME/$CMPNTNAME.js
+		echo "import classes from './$CMPNTNAME.module.scss';" >> $CMPNTDIR/$CMPNTNAME/$CMPLNAME
 		echo ".$CMPNTNAME {
 }" >> $CMPNTDIR/$CMPNTNAME/$CMPNTNAME.module.scss
 		code $CMPNTDIR/$CMPNTNAME/$CMPNTNAME.module.scss
@@ -37,7 +44,7 @@ class $CMPNTNAME extends Component {
 		)
 	}
 }
-export default $CMPNTNAME;" >> $CMPNTDIR/$CMPNTNAME/$CMPNTNAME.js
+export default $CMPNTNAME;" >> $CMPNTDIR/$CMPNTNAME/$CMPLNAME
 	fi
 	if [ "$CMPNTTYPE" == "function" ]; then
 		echo "const $CMPNTNAME = () => {
@@ -46,6 +53,6 @@ export default $CMPNTNAME;" >> $CMPNTDIR/$CMPNTNAME/$CMPNTNAME.js
 	)
 }
 
-export default $CMPNTNAME;" >> $CMPNTDIR/$CMPNTNAME/$CMPNTNAME.js
+export default $CMPNTNAME;" >> $CMPNTDIR/$CMPNTNAME/$CMPLNAME
 	fi
 fi
